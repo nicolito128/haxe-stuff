6b030ea482b38cc40417227e71818c299f434133
Nicolás Serna <nicolito128@hotmail.com>
2022-08-18 14:08:02 -0300

    Add js

---
+++ js/server-test/main.js
@@ -0,0 +1,52 @@
+(function ($global) { "use strict";
+var Main = function() { };
+Main.main = function() {
+	var server = js_node_Http.createServer(function(request,response) {
+		response.writeHead(200,{ "Content-Type" : "text/plain"});
+		response.end("Hello, Haxe World\n");
+	});
+	server.listen(8000);
+	console.log("Main.hx:13:","Server running at 127.0.0.1:8000");
+};
+var haxe_iterators_ArrayIterator = function(array) {
+	this.current = 0;
+	this.array = array;
+};
+haxe_iterators_ArrayIterator.prototype = {
+	hasNext: function() {
+		return this.current < this.array.length;
+	}
+	,next: function() {
+		return this.array[this.current++];
+	}
+};
+var js_node_Http = require("http");
+var js_node_KeyValue = {};
+js_node_KeyValue.get_key = function(this1) {
+	return this1[0];
+};
+js_node_KeyValue.get_value = function(this1) {
+	return this1[1];
+};
+var js_node_stream_WritableNewOptionsAdapter = {};
+js_node_stream_WritableNewOptionsAdapter.from = function(options) {
+	if(!Object.prototype.hasOwnProperty.call(options,"final")) {
+		Object.defineProperty(options,"final",{ get : function() {
+			return options.final_;
+		}});
+	}
+	return options;
+};
+var js_node_url_URLSearchParamsEntry = {};
+js_node_url_URLSearchParamsEntry._new = function(name,value) {
+	var this1 = [name,value];
+	return this1;
+};
+js_node_url_URLSearchParamsEntry.get_name = function(this1) {
+	return this1[0];
+};
+js_node_url_URLSearchParamsEntry.get_value = function(this1) {
+	return this1[1];
+};
+Main.main();
+})({});
